ac88998b5cf0558a3b77e451dac8ee16ff23b8e6
郑杰 <411426414@qq.com>
2020-09-03 01:00:39 +0800

    利用路由导航守卫控制页面权限

--- vue_shop/src/router/index.js
+++ vue_shop/src/router/index.js
@@ -1,17 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  /* 路由重定向 */
-  { path: '/', redirect: '/login' },
-  { path: '/login', component: Login }
-]
-
 const router = new VueRouter({
-  routes
+  routes: [
+    /* 路由重定向 */
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
+  ]
+})
+
+// 挂载路由导航守卫
+//  to将要访问的路径
+//  from代表从哪个路径跳转而来
+//  next是一个函数，表示放行
+//      next() 放行   next(' /login')强制跳转
+router.beforeEach((to, from, next) => {
+  // 如果用户访问的登录页，直接放行
+  if (to.path === '/login') return next()
+  // 从sessionStorage中获取到保存的token值
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 没有token，，强制跳转到登录页
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
