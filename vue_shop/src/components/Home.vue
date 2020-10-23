c12b981da076179b9610be4a76bedd38e8c19dc2
郑杰 <411426414@qq.com>
2020-10-23 21:00:02 +0800

    添加头部区域

--- vue_shop/src/components/Home.vue
+++ vue_shop/src/components/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="">
+        <span>电商后台管理系统</span>
+      </div>
       <el-button type="info" @click='logout'>退出</el-button>
+    </el-header>
   </el-container>
 </template>
 
@@ -16,4 +23,26 @@ export default {
 </script>
 
 <style lang="less">
+  .home-container {
+    height: 100%;
+  }
+
+  .el-header {
+    background-color: #373D41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0 !important;
+    align-items: center;
+    color: #FFF;
+    font-size: 20px;
+
+    > div {
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-left: 15px;
+      }
+    }
+  }
 </style>
