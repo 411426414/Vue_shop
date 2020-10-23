78e64197c3f3c9806f02c158c12b4f7b1f4cb0dd
郑杰 <411426414@qq.com>
2020-10-23 20:58:39 +0800

    添加退出按钮

--- vue_shop/src/components/Home.vue
+++ vue_shop/src/components/Home.vue
@@ -1,17 +1,15 @@
 <template>
-  <div>
-    <el-button type="info" @click='logout'>退出</el-button>
-  </div>
+  <el-container class="home-container">
+      <el-button type="info" @click='logout'>退出</el-button>
+  </el-container>
 </template>
 
 <script>
 export default {
   methods: {
-    logout () {
-      // 清空token
-      window.sessionStorage.clear()
-      // 跳转到登录页面
-      this.$router.push('/login')
+    logout() {
+      window.sessionStorage.clear()// 清空token
+      this.$router.push('/login') // 跳转到登录页面
     }
   }
 }
