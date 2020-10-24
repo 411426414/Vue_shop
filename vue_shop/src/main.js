cc0dd0b7abd9ad502ddc2825a5440fcee2f0cf36
郑杰 <411426414@qq.com>
2020-10-24 23:08:19 +0800

    添加axios.interceptors 拦截器，拦截请求前和请求后

--- vue_shop/src/main.js
+++ vue_shop/src/main.js
@@ -10,11 +10,9 @@ import router from './router'
 
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
-axios.interceptors.request.use(config => {
-  // console.log(config)
-  config.headers.Authorization = window.sessionStorage.getItem('token')
-  // 在最后必须 return config
-  return config
+axios.interceptors.request.use(config => { //添加axios的axios.interceptors 拦截器，拦截请求前和请求后
+  config.headers.Authorization = window.sessionStorage.getItem('token') // console.log(config)
+  return config // 在最后必须 return config
 })
 Vue.prototype.$http = axios
 
