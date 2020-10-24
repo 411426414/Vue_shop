34f7090652d71ed32d707f30d9e981b4b151ea3b
郑杰 <411426414@qq.com>
2020-10-24 22:57:24 +0800

    添加Card 卡片，将信息聚合在卡片容器中展示。

--- vue_shop/src/plugins/element.js
+++ vue_shop/src/plugins/element.js
@@ -1,4 +1,4 @@
-import { Aside, Breadcrumb, BreadcrumbItem, Button, Container, Form, FormItem, Header, Input, Main, Menu, MenuItem, Message, Submenu } from 'element-ui'
+import { Aside, Breadcrumb, BreadcrumbItem, Button, Card, Container, Form, FormItem, Header, Input, Main, Menu, MenuItem, Message, Submenu } from 'element-ui'
 import Vue from 'vue'
 
 Vue.use(Button)
@@ -14,4 +14,5 @@ Vue.use(Submenu)
 Vue.use(MenuItem)
 Vue.use(Breadcrumb)
 Vue.use(BreadcrumbItem)
+Vue.use(Card)
 Vue.prototype.$message = Message
