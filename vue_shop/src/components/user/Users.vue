f08ae89176929d78aa2d74e14ecd485c8a2037ee
郑杰 <411426414@qq.com>
2020-10-24 22:53:40 +0800

    添加面包屑导航

--- vue_shop/src/components/user/Users.vue
+++ vue_shop/src/components/user/Users.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <h3>用户列表组件</h3>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
   </div>
 </template>
 
 <script>
-  export default {}
+export default {}
 </script>
 
 <style lang="less" scoped>
