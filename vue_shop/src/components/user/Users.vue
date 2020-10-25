b6c1750e6f25cfad655e88bba74d28f7cbca5dd2
郑杰 <411426414@qq.com>
2020-10-25 23:51:56 +0800

    获取用户列表的参数

--- vue_shop/src/components/user/Users.vue
+++ vue_shop/src/components/user/Users.vue
@@ -9,8 +9,10 @@
     <!-- 卡片视图区域 -->
     <el-card class="box-card">
       <!-- 搜索与添加区域 -->
-      <el-row :gutter="20"> <!-- gutter 间隔 -->
-        <el-col :span="8"><!-- span 列的宽度 -->
+      <el-row :gutter="20">
+        <!-- gutter 间隔 -->
+        <el-col :span="8"
+          ><!-- span 列的宽度 -->
           <el-input placeholder="请输入内容">
             <el-button slot="append" icon="el-icon-search"></el-button>
           </el-input>
@@ -24,7 +26,32 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      // 获取用户列表的参数对象
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 2
+      }
+    }
+  },
+  created() {
+    this.getUserList()
+  },
+  methods: {
+    async getUserList() {
+      const { data: res } = await this.$http.get('users', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取用户列表失败！')
+      }
+      console.log(res)
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
