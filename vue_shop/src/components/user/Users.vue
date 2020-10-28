801a6f2bad1027c2e51effc4adac1c4748ba5158
郑杰 <411426414@qq.com>
2020-10-28 23:52:12 +0800

    可以发起修改用户的网络请求

--- vue_shop/src/components/user/Users.vue
+++ vue_shop/src/components/user/Users.vue
@@ -98,7 +98,7 @@
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="editDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="editDialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="editUserInfo">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -253,6 +253,13 @@ export default {
     // 监听修改用户对话框的关闭事件
     editDialogClosed() {
       this.$refs.editFormRef.resetFields()
+    },
+    // 修改用户信息并提交
+    editUserInfo() {
+      this.$refs.editFormRef.validate(valid => {
+        if (valid) return
+        // 可以发起修改用户的网络请求
+      })
     }
   }
 }
