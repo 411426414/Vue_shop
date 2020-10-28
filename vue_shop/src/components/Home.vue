057fc9dd95d8c9aa03659fec520ea55c49df4afc
郑杰 <411426414@qq.com>
2020-10-28 23:54:15 +0800

    获取所有的菜单

--- vue_shop/src/components/Home.vue
+++ vue_shop/src/components/Home.vue
@@ -79,7 +79,7 @@ export default {
       const { data: res } = await this.$http.get('menus')
       if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
       this.menuList = res.data
-      console.log(res)
+      // console.log(res)
       console.log(this.menuList)
     },
     // 点击按钮的切换和展开
