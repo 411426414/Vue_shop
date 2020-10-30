2c5bd5f6f8e7fc3d356ecec717ef41309308f772
郑杰 <411426414@qq.com>
2020-10-30 23:46:35 +0800

    添加角色按钮

--- vue_shop/src/components/power/Roles.vue
+++ vue_shop/src/components/power/Roles.vue
@@ -6,6 +6,15 @@
       <el-breadcrumb-item>权限管理</el-breadcrumb-item>
       <el-breadcrumb-item>角色列表</el-breadcrumb-item>
     </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 添加角色按钮 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加角色</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
