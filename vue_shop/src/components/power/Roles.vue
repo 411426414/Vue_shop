28d98b613fc6d90be79fcf71fd0cf1ac6db5dc5a
郑杰 <411426414@qq.com>
2020-10-30 23:45:07 +0800

    面包屑导航区域

--- vue_shop/src/components/power/Roles.vue
+++ vue_shop/src/components/power/Roles.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>角色列表</div>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="less" scope>
+</style>
