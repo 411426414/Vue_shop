2881d5cb06022a7c18feba3e3e8c4528d1173104
郑杰 <411426414@qq.com>
2020-11-05 22:22:08 +0800

    使用template指定一个作用域插槽

--- vue_shop/src/components/goods/Cate.vue
+++ vue_shop/src/components/goods/Cate.vue
@@ -33,7 +33,12 @@
         :expand-type="false"
         :selection-type="false"
         :show-row-hover="false"
-      ></tree-table>
+      >
+        <template slot="isOk" slot-scope="scope">
+          <i class="el-icon-success" v-if="scope.row.cat_deleted === false" style="color: lightgreen"></i>
+          <i class="el-icon-error" v-else  style="color: red "></i>
+        </template>
+      </tree-table>
       <!-- 分页区域 -->
     </el-card>
   </div>
@@ -58,6 +63,13 @@ export default {
         {
           label: '分类名称',
           prop: 'cat_name'
+        },
+        {
+          label: '是否有效',
+          // 表示当前列定义为模板列
+          type: 'template',
+          // 表示当前这一列使用的名称,使用template指定一个作用域插槽
+          template: 'isOk'
         }
       ]
     }
