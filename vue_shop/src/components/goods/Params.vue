8fa16d62a85e201cb81fd3893ff4117beed3a1c0
郑杰 <411426414@qq.com>
2020-11-09 23:31:15 +0800

    选择商品分类区域

--- vue_shop/src/components/goods/Params.vue
+++ vue_shop/src/components/goods/Params.vue
@@ -9,6 +9,14 @@
     <!-- 卡片视图区域 -->
     <el-card>
       <el-alert show-icon title="注意：只允许为第三级分类设置相关参数！" type="warning" :closable="false"></el-alert>
+      <!-- 选择商品分类区域 -->
+      <el-row class="cat_opt">
+        <el-col>
+          <span>选择商品分类：</span>
+          <!-- 选择商品分类的级联选择框 -->
+          <el-cascader></el-cascader>
+        </el-col>
+      </el-row>
     </el-card>
   </div>
 </template>
@@ -24,4 +32,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
+  .cat_opt {
+    margin: 15px 0;
+  }
 </style>
