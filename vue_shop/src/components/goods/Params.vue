99988ae96adc705256962e5276913a6ab7ee3436
郑杰 <411426414@qq.com>
2020-11-09 23:19:56 +0800

    Alert 警告：用于页面中展示重要的提示信息。

--- vue_shop/src/components/goods/Params.vue
+++ vue_shop/src/components/goods/Params.vue
@@ -7,7 +7,9 @@
       <el-breadcrumb-item>参数列表</el-breadcrumb-item>
     </el-breadcrumb>
     <!-- 卡片视图区域 -->
-    <el-card></el-card>
+    <el-card>
+      <el-alert title="注意：只允许为第三级分类设置相关参数！" type="warning" show-icon> </el-alert>
+    </el-card>
   </div>
 </template>
 
