f36f29201145e9a66d0e03c3717ebf571976a3aa
郑杰 <411426414@qq.com>
2020-11-09 23:23:13 +0800

    自定义关闭按钮为文字或其他符号。

--- vue_shop/src/components/goods/Params.vue
+++ vue_shop/src/components/goods/Params.vue
@@ -8,7 +8,7 @@
     </el-breadcrumb>
     <!-- 卡片视图区域 -->
     <el-card>
-      <el-alert title="注意：只允许为第三级分类设置相关参数！" type="warning" show-icon> </el-alert>
+      <el-alert title="注意：只允许为第三级分类设置相关参数！" type="warning" show-icon :closable="false"></el-alert>
     </el-card>
   </div>
 </template>
