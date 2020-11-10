1dd6c4c8253ef8ae804499a349af6471b45c14e2
郑杰 <411426414@qq.com>
2020-11-10 23:20:31 +0800

    被激活的页签的名称

--- vue_shop/src/components/goods/Params.vue
+++ vue_shop/src/components/goods/Params.vue
@@ -24,7 +24,7 @@
           </el-cascader>
         </el-col>
       </el-row>
-      <el-tabs @tab-click="handleClick">
+      <el-tabs v-model="activeName" @tab-click="handleClick">
         <el-tab-pane label="动态参数" name="first">动态参数</el-tab-pane>
         <el-tab-pane label="静态属性" name="second">静态属性</el-tab-pane>
       </el-tabs>
@@ -44,7 +44,9 @@ export default {
         children: 'children'
       },
       // 级联选择框双向绑定的数组
-      selectedCateKeys: []
+      selectedCateKeys: [],
+      // 被激活的页签的名称
+      activeName: 'first'
     }
   },
   created() {
