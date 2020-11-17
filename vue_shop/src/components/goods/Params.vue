7a65a0d4a4f953285585f6dc0db64d90be435686
郑杰 <411426414@qq.com>
2020-11-17 22:10:42 +0800

    清空动态参数或静态属性

--- vue_shop/src/components/goods/Params.vue
+++ vue_shop/src/components/goods/Params.vue
@@ -199,6 +199,8 @@ export default {
       // 证明选中的不是三级分类
       if (this.selectedCateKeys.length !== 3) {
         this.selectedCateKeys = []
+        this.manyTableData = []
+        this.onlyTableData = []
         return false
       }
       // 证明选中的是三级分类
