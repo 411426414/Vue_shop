1f341151515e8417719de641c79f3ac995253e6f
郑杰 <411426414@qq.com>
2020-11-18 21:52:07 +0800

    修改表格列的大小

--- vue_shop/src/components/goods/List.vue
+++ vue_shop/src/components/goods/List.vue
@@ -21,10 +21,10 @@
       <el-table :data='goodsList' border stripe>
         <el-table-column type="index"></el-table-column>
         <el-table-column label="商品名称" prop="goods_name"></el-table-column>
-        <el-table-column label="商品价格(元)" prop="goods_price"></el-table-column>
-        <el-table-column label="商品重量" prop="goods_weight"></el-table-column>
-        <el-table-column label="创建时间" prop="add_time"></el-table-column>
-        <el-table-column label="操作">
+        <el-table-column label="商品价格(元)" prop="goods_price" width="105px"></el-table-column>
+        <el-table-column label="商品重量" prop="goods_weight" width="80px"></el-table-column>
+        <el-table-column label="创建时间" prop="add_time" width="140px"></el-table-column>
+        <el-table-column label="操作" width="130px">
           <template>
             <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
             <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
