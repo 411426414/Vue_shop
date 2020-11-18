a4ba287e773c08c1e19b62f8a77d097b44f83aea
郑杰 <411426414@qq.com>
2020-11-18 22:04:03 +0800

    使用自定义过滤器,优化时间

--- vue_shop/src/components/goods/List.vue
+++ vue_shop/src/components/goods/List.vue
@@ -23,7 +23,11 @@
         <el-table-column label="商品名称" prop="goods_name"></el-table-column>
         <el-table-column label="商品价格(元)" prop="goods_price" width="105px"></el-table-column>
         <el-table-column label="商品重量" prop="goods_weight" width="80px"></el-table-column>
-        <el-table-column label="创建时间" prop="add_time" width="140px"></el-table-column>
+        <el-table-column label="创建时间" prop="add_time" width="160px">
+          <template slot-scope="scope">
+            {{scope.row.add_time | dateFormat}}
+          </template>
+        </el-table-column>
         <el-table-column label="操作" width="130px">
           <template>
             <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
