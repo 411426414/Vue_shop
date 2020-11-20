d6392dc03b627b90b344610d9617e62997bc0dff
郑杰 <411426414@qq.com>
2020-11-20 22:38:22 +0800

    定义删除函数弹出对话框

--- vue_shop/src/components/goods/List.vue
+++ vue_shop/src/components/goods/List.vue
@@ -92,6 +92,13 @@ export default {
     handleCurrentChange(newPage) {
       this.queryInfo.pagenum = newPage
       this.getGoodsList()
+    },
+    async removeById(id) {
+      const confirmResult = await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
     }
   }
 }
