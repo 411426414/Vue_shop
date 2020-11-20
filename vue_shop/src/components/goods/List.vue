78ffc2210c120071cb119cf075c3d5cf75732673
郑杰 <411426414@qq.com>
2020-11-20 22:39:41 +0800

    提交删除商品的请求

--- vue_shop/src/components/goods/List.vue
+++ vue_shop/src/components/goods/List.vue
@@ -102,6 +102,11 @@ export default {
       if (confirmResult !== 'confirm') {
         return this.$message.info('已经取消删除')
       }
+      const { data: res } = await this.$http.delete(`goods/${id}`)
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除失败！')
+      }
+      this.$message.success('删除成功！')
     }
   }
 }
