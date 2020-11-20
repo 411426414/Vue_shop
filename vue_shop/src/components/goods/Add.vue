c6892667fa026f11fe4fcda203006b2b93772324
郑杰 <411426414@qq.com>
2020-11-20 22:54:49 +0800

    Steps 步骤条： 含状态步骤条

--- vue_shop/src/components/goods/Add.vue
+++ vue_shop/src/components/goods/Add.vue
@@ -17,6 +17,12 @@
         :closable="false"
       >
       </el-alert>
+      <!-- Steps 步骤条： 含状态步骤条 -->
+      <el-steps :space="200" :active="1" finish-status="success">
+        <el-step title="已完成"></el-step>
+        <el-step title="进行中"></el-step>
+        <el-step title="步骤 3"></el-step>
+      </el-steps>
     </el-card>
   </div>
 </template>
