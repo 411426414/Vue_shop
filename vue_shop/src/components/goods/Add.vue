d4777c5ebcbcb8b355c95ecf8d6b6f226430ff92
郑杰 <411426414@qq.com>
2020-11-24 21:40:01 +0800

    绑定选中选项卡的 name 值

--- vue_shop/src/components/goods/Add.vue
+++ vue_shop/src/components/goods/Add.vue
@@ -32,12 +32,12 @@
         <el-step title="完成"></el-step>
       </el-steps>
       <!-- tab栏区域 -->
-      <el-tabs tab-position="left" style="height: 200px">
-        <el-tab-pane label="基本信息">基本信息</el-tab-pane>
-        <el-tab-pane label="商品参数">商品参数</el-tab-pane>
-        <el-tab-pane label="商品属性">商品属性</el-tab-pane>
-        <el-tab-pane label="商品图片">商品图片</el-tab-pane>
-        <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+      <el-tabs v-model="activeIndex" tab-position="left" style="height: 200px">
+        <el-tab-pane name="0" label="基本信息">基本信息</el-tab-pane>
+        <el-tab-pane name="1" label="商品参数">商品参数</el-tab-pane>
+        <el-tab-pane name="2" label="商品属性">商品属性</el-tab-pane>
+        <el-tab-pane name="3" label="商品图片">商品图片</el-tab-pane>
+        <el-tab-pane name="4" label="商品内容">商品内容</el-tab-pane>
       </el-tabs>
     </el-card>
   </div>
@@ -47,7 +47,7 @@
 export default {
   data() {
     return {
-      activeIndex: 0
+      activeIndex: '0'
     }
   },
   created() {},
