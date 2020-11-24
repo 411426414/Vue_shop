ab2aad6061327637d4570f984af34a5b62ff4ffa
郑杰 <411426414@qq.com>
2020-11-24 22:01:10 +0800

    添加数据验证规则

--- vue_shop/src/components/goods/Add.vue
+++ vue_shop/src/components/goods/Add.vue
@@ -79,7 +79,20 @@ export default {
         goods_weight: 0,
         goods_number: 0
       },
-      addFormRules: {}
+      addFormRules: {
+        goods_name: [
+          { required: true, message: '请输入商品名称', trigger: 'blur' }
+        ],
+        goods_price: [
+          { required: true, message: '请输入商品价格', trigger: 'blur' }
+        ],
+        goods_weight: [
+          { required: true, message: '请输入商品重量', trigger: 'blur' }
+        ],
+        goods_number: [
+          { required: true, message: '请输入商品数量', trigger: 'blur' }
+        ]
+      }
     }
   },
   created() {},
