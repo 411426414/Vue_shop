97bb2088456ccde86a320defdb36d8e6336ba26f
郑杰 <411426414@qq.com>
2020-11-30 22:44:36 +0800

    导入富文本编辑器及对应的样式

--- vue_shop/src/main.js
+++ vue_shop/src/main.js
@@ -1,5 +1,12 @@
 import axios from 'axios'
+// require styles 导入富文本编辑器对应的样式
+import 'quill/dist/quill.bubble.css'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
 import Vue from 'vue'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 树形表格treeTable实现了layui数据表格的大部分功能，并且在用法上与几乎数据表格一致， 支持懒加载、复选框联动(半选)、拖拽列宽、固定表头等功能。
 import TreeTable from 'vue-table-with-tree-grid'
 import App from './App.vue'
 // 导入全局样式表
@@ -8,6 +15,9 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import './plugins/element.js'
 import router from './router'
+
+// 将富文本编辑器，注册为全局可以组件
+Vue.use(VueQuillEditor)
 
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
