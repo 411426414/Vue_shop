a172976b8f9d394e753b3ad206b17dfc197445f2
411426414 <411426414@qq.com>
2020-12-05 22:59:12 +0800

    添加订单路由

--- vue_shop/src/router/index.js
+++ vue_shop/src/router/index.js
@@ -6,6 +6,7 @@ import GoodsList from '../components/goods/List.vue'
 import Params from '../components/goods/Params.vue'
 import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
+import Orders from '../components/order/Order.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
 import Users from '../components/user/Users.vue'
@@ -14,26 +15,27 @@ import Welcome from '../components/Welcome.vue'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: [
-    /* 路由重定向 */
-    { path: '/', redirect: '/login' },
-    { path: '/login', component: Login },
-    {
-      path: '/home',
-      component: Home,
-      redirect: '/welcome',
-      children: [
-        { path: '/welcome', component: Welcome },
-        { path: '/users', component: Users },
-        { path: '/rights', component: Rights },
-        { path: '/roles', component: Roles },
-        { path: '/categories', component: Cate },
-        { path: '/params', component: Params },
-        { path: '/goods', component: GoodsList },
-        { path: '/goods/add', component: Add }
-      ]
-    }
-  ]
+    routes: [
+        /* 路由重定向 */
+        { path: '/', redirect: '/login' },
+        { path: '/login', component: Login },
+        {
+            path: '/home',
+            component: Home,
+            redirect: '/welcome',
+            children: [
+                { path: '/welcome', component: Welcome },
+                { path: '/users', component: Users },
+                { path: '/rights', component: Rights },
+                { path: '/roles', component: Roles },
+                { path: '/categories', component: Cate },
+                { path: '/params', component: Params },
+                { path: '/goods', component: GoodsList },
+                { path: '/goods/add', component: Add },
+                { path: '/orders', component: Orders }
+            ]
+        }
+    ]
 })
 
 // 挂载路由导航守卫
@@ -42,13 +44,13 @@ const router = new VueRouter({
 //  next是一个函数，表示放行
 //      next() 放行   next(' /login')强制跳转
 router.beforeEach((to, from, next) => {
-  // 如果用户访问的登录页，直接放行
-  if (to.path === '/login') return next()
-  // 从sessionStorage中获取到保存的token值
-  const tokenStr = window.sessionStorage.getItem('token')
-  // 没有token，，强制跳转到登录页
-  if (!tokenStr) return next('/login')
-  next()
+    // 如果用户访问的登录页，直接放行
+    if (to.path === '/login') return next()
+        // 从sessionStorage中获取到保存的token值
+    const tokenStr = window.sessionStorage.getItem('token')
+        // 没有token，，强制跳转到登录页
+    if (!tokenStr) return next('/login')
+    next()
 })
 
 export default router
