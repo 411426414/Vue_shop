25eeb491d5a560fc095a9ab2ab3402ba327c896c
411426414 <411426414@qq.com>
2020-12-05 22:58:46 +0800

    卡片图片区域

--- vue_shop/src/components/order/Order.vue
+++ vue_shop/src/components/order/Order.vue
@@ -6,6 +6,17 @@
       <el-breadcrumb-item>订单管理</el-breadcrumb-item>
       <el-breadcrumb-item>订单列表</el-breadcrumb-item>
     </el-breadcrumb>
+
+    <!-- 卡片图片区域 -->
+    <el-card>
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
