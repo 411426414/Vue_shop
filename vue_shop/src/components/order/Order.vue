55f916aabb1e9603bee17a1a247d93142e9ad51c
411426414 <411426414@qq.com>
2020-12-09 23:19:31 +0800

    添加带索引列的订单列表数据

--- vue_shop/src/components/order/Order.vue
+++ vue_shop/src/components/order/Order.vue
@@ -16,6 +16,11 @@
           </el-input>
         </el-col>
       </el-row>
+
+      <!-- 订单列表数据 -->
+      <el-table :data="orderlist" border stripe>
+        <el-table-column type="index"></el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
