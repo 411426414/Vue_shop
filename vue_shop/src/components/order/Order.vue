45e2814542b7b808bcd5cb430fbabf44a1aa7de0
411426414 <411426414@qq.com>
2020-12-10 23:01:19 +0800

    组件布局，子组件名用逗号分隔

--- vue_shop/src/components/order/Order.vue
+++ vue_shop/src/components/order/Order.vue
@@ -62,6 +62,7 @@
         @current-change="handleCurrentChange"
         :current-page="queryInfo.pagenum"
         :page-sizes="[5, 10, 15, 20]"
+        layout="total, sizes, prev, pager, next, jumper"
       >
       </el-pagination>
     </el-card>
