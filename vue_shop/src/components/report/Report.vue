30f7616c6a5c81ab5a78451ff10b4e405cdaa434
411426414 <411426414@qq.com>
2020-12-12 18:30:27 +0800

    添加面包屑导航

---
+++ vue_shop/src/components/report/Report.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      key: value
+    }
+  },
+  created () {
+    
+  },
+  methods: {
+    name() {
+      
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
