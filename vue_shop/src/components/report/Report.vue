93239f50a110b4abdc7a9444795309e447201b06
411426414 <411426414@qq.com>
2020-12-13 21:19:38 +0800

    发生请求获取折线图数据并添加该页面

--- vue_shop/src/components/report/Report.vue
+++ vue_shop/src/components/report/Report.vue
@@ -9,51 +9,31 @@
     <!-- 卡片视图区域 -->
     <el-card>
       <!-- 2. 为ECharts准备一个具备大小（宽高）的Dom -->
-      <div id="main" style="width: 600px; height: 400px"></div>
+      <div id="main" style="width: 750px; height: 400px"></div>
     </el-card>
   </div>
 </template>
 
 <script>
 // 1. 导入echarts
-var echarts = require('echarts');
+var echarts = require('echarts')
 export default {
   data() {
     return {}
   },
   created() {},
   // 此时页面上的元素，已经渲染完毕
-  mounted() {
+  async mounted() {
     // 3. 基于准备好的dom，初始化echarts实例
     var myChart = echarts.init(document.getElementById('main'))
+
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取折线图数据失败！')
+    }
     // 4. 准备数据和配置项
-    var option = {
-      title: {
-        text: 'ECharts 入门示例',
-      },
-      tooltip: {},
-      legend: {
-        data: ['销量'],
-      },
-      xAxis: {
-        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
-      },
-      yAxis: {},
-      series: [
-        {
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20],
-        },
-      ],
-    }
     // 5. 展示数据
-    myChart.setOption(option)
-  },
-  methods: {
-    name() {
-      
-    }
+    myChart.setOption(res.data)
   },
 }
 </script>
